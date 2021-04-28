16e50b62f8a531f0d95a3c4e7317c703c51576a2
Your Name <sam.heyman@gmail.com>
2021-04-28 21:12:54 +0200

    Add php script to send contact form

---
+++ src/send_email.php
@@ -0,0 +1,111 @@
+<?php
+if (isset($_POST['email'])) {
+
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "info@biabint.se";
+    $email_subject = "New message";
+
+    function problem($error)
+    {
+        echo "We are sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br><br>";
+        echo $error . "<br><br>";
+        echo "Please go back and fix these errors.<br><br>";
+        die();
+    }
+
+    // validation expected data exists
+    if (
+        !isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['subject']) ||
+        !isset($_POST['message'])
+    ) {
+        problem('We are sorry, there was a problem with the form you submitted.');
+    }
+
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $subject = $_POST['subject']; // required
+    $message = $_POST['message']; // required
+
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered is not valid.<br>';
+    }
+
+    $string_exp = "/^[A-Za-z .'-]+$/";
+
+    if (!preg_match($string_exp, $name)) {
+        $error_message .= 'The Name you entered does not appear to be valid.<br>';
+    }
+
+    if (strlen($message) < 2) {
+        $error_message .= 'The Message you entered do not appear to be valid.<br>';
+    }
+
+    if (strlen($error_message) > 0) {
+        problem($error_message);
+    }
+
+    $email_message = "You have received a message from the website.\n\n";
+
+    function clean_string($string)
+    {
+        $bad = array("content-type", "bcc:", "to:", "cc:", "href");
+        return str_replace($bad, "", $string);
+    }
+
+    $email_message .= "Name: " . clean_string($name) . "\n";
+    $email_message .= "Email: " . clean_string($email) . "\n";
+    $email_message .= "Subject: " . clean_string($subject) . "\n";
+    $email_message .= "Message: " . clean_string($message) . "\n";
+
+    // create email headers
+    $headers = 'From: info@biabint.se' . "\r\n" .
+    'Reply-To: info@biabint.se' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+    @mail($email_to, $email_subject, $email_message, $headers);
+?>
+<!DOCTYPE HTML>
+<!--
+	Twenty by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Biab Innovation</title>
+		<meta charset="utf-8" />
+		<meta
+		  name="viewport"
+		  content="width=device-width, initial-scale=1, user-scalable=no"
+		/>
+	  </head>
+	<body class="contact is-preload">
+		<div id="page-wrapper">
+
+			
+
+			
+		
+
+							
+								<div class="content">
+    Thank you for contacting us. We will be in touch with you very soon.
+
+<br />
+<br />
+    <a href="https://biabint.se">Back to homepage</a>
+</div>
+</section>
+</article>
+</div>
+</body>
+</html>
+
+<?php
+}
+?>
